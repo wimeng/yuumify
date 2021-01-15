efc6e4fe88baf4e52b4cddcb8749152373bff783
wimeng <wimeng@umich.edu>
2021-01-14 19:21:08 -0500

    adding new communication tests

---
+++ clienttest.py
@@ -0,0 +1,17 @@
+# Import socket module
+import socket
+
+# Create a socket object
+s = socket.socket()
+print ('kdjsafhklajsfh\n')
+# Define the port on which you want to connect
+port = 12345
+
+# connect to the server on local computer
+s.connect(('127.0.0.1', port))
+
+# receive data from the server
+message = s.recv(1024)
+print (str(message))
+# close the connection
+s.close()
--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import sys
 import pyautogui
 import sockets
 
